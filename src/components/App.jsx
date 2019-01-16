5578a3451e696b0d13713d82b515074eb0e3a6ad
Shakib Hossain <shakib609@gmail.com>
2019-01-16 12:18:16 +0600

    Add dotenv package and implement unsplash api client

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar'
 
 export default class App extends React.Component {
-  onSearchSubmit (term) {
-    console.log(term)
+  state = { images: [] }
+
+  onSearchSubmit = async term => {
+    const response = await unsplash.get(`/search/photos`, {
+      params: { query: term }
+    })
+    this.setState({ images: response.data.results })
   }
 
   render () {
     return <div className='ui container' style={{ marginTop: '30px' }}>
       <SearchBar onSubmit={this.onSearchSubmit} />
+      <p>Found {this.state.images.length} images.</p>
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import dotenv from 'dotenv'
 import App from './components/App'
+
+dotenv.config({
+  path: '../.env.local'
+})
+console.log(process.env.REACT_APP_UNSPLASH_ACCESS_KEY)
 
 ReactDOM.render(
   <App />,
